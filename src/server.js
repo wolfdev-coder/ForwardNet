1f25557c97fc0e8bea1b77a4b0b5cd805ae44e0b
wolfdev-coder <maximkaplay67@gmail.com>
2025-02-11 01:11:25 +0400

    Coding...

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+    console.log(`Сайт доступен по адресу: http://localhost:${port}`);
+});
